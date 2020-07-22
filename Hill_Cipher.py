a437a9027fea1fe30a8d524e48cc0c527f7172a0
guasonito <mdavidcuello@gmail.com>
2020-07-22 11:16:56 -0500

    versio en proceso

---
+++ Hill_Cipher.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+Letters={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25}
+#plaintext=input("Palabra a cifrar:")
+plaintext="hola"
+plaintext=list(plaintext)
+plaintext_numbers=[]
+key_numbers=[]
+#Key=input("Palabra llave:")
+key="cehbcbdbf"
+key=list(key)
+for j in range(len(plaintext)):
+    for i in Letters.items():
+        if i[0]==plaintext[j]:
+            plaintext_numbers.append(i[1])
+for j in range(len(key)): 
+    for i in Letters.items():           
+        if i[0]==key[j]:
+            key_numbers.append(i[1])
+square=int(math.sqrt(len(key_numbers)))
+key_matrix=np.array(key_numbers).reshape(square,square)
+print(key_matrix)
+print(int(np.linalg.det(key_matrix)))
