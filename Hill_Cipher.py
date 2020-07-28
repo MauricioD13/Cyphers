abd08768a00e592254f2263ed942bb9e6522cd2e
guasonito <mdavidcuello@gmail.com>
2020-07-28 14:21:36 -0500

    Encripcion, desencripcion y menu completo version 2

--- Hill_Cipher.py
+++ Hill_Cipher.py
@@ -17,6 +17,14 @@ def text_issue(text,square):
 
 
 #Preparation for Encription 
+#This function return an array with this information: 
+    # square            0 : This is the lenght of the square matrix
+    # lenght_plaintext  1 : 
+    # text_numbers      2
+    # key_matrix        3
+    # invert_number     4
+    # determinant       5
+    # lenght_text       6
 def preparation(key,text,option):
     #The key,cyphertext and option must be a string 
     key_numbers=[]
@@ -30,10 +38,7 @@ def preparation(key,text,option):
             if i[0]==key[j]:
                 key_numbers.append(i[1])
     information.append(int(math.sqrt(len(key_numbers)))) #square 0
-    if option=="encrypt":
-        information.append(text_issue(text,information[0])) #lenght_plaintext 1
-    else:
-        information.append(len(text))
+    information.append(text_issue(text,information[0])) #lenght_plaintext 
     for j in range(len(text)):
         for i in Letters.items():
          if i[0]==text[j]:
@@ -47,22 +52,20 @@ def preparation(key,text,option):
     #print(invert_number)
     information.append(invert_number)
     information.append(det_key_matrix-1)
-    # Contain of the array information: 
-    # square            0
-    # lenght_plaintext  1
-    # text_numbers      2
-    # key_matrix        3
-    # invert_number     4
-    # determinant       5
-    # lenght_text       6
+    information.append(lenght_text)
     return information
 
 #Encryption
+#This function needs the next information that provides the function "preparation"
 def Encryption(information):
+    #Change variable´s name
     square=information[0]
     lenght_plaintext=information[1]
     plaintext_numbers=information[2]
     key_matrix=information[3]
+    lenght_text=information[6]
+
+    #Process of Encryption
     words=int(lenght_plaintext/square)
     array_plaintext=[]
     for i in range(0,words):
@@ -82,14 +85,17 @@ def Encryption(information):
     return encript_message
 
 #Decryption 
+#This function needs the next information that provides the function "preparation"
+#Return a string 
 def Decryption(information):
-    # Varible name change
+    #Change variable´s name
     square=information[0]
     lenght_plaintext=information[1]
     plaintext_numbers=information[2]
     key_matrix=information[3]
     inverse_number=information[4]
     det_key_matrix=information[5]
+    lenght_text=information[6]
     
     #Process of decryption
     inverse=np.linalg.inv(key_matrix)
@@ -110,8 +116,9 @@ def Decryption(information):
             for i in Letters.items():
                     if i[1]==result[j]:
                         res.append(i[0])
+    res=res[0:lenght_text]
     decrypt_message=""
-    for i in range(0,lenght_plaintext):
+    for i in range(0,lenght_text):
         decrypt_message=decrypt_message+res[i]
     return(decrypt_message)
    
@@ -139,10 +146,11 @@ def menu(switcher):
 Letters={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25}
 
 
-print("1. Encriptar mensaje")
-print("2. Desencriptar mensaje")
+
 repeat="si"
 while repeat=="si":
+    print("1. Encriptar mensaje")
+    print("2. Desencriptar mensaje")
     switcher=int(input("Que opcion desea: ") )  
     menu(switcher)
     repeat=input("Otra vez?(si/no): ")
--- inversion_modular.py
+++ inversion_modular.py
@@ -2,3 +2,5 @@ def module_invert(determinant):
     for i in range(0,abs(determinant)+1):
         if (determinant*i)%26==1:
             return i
+    else:
+        print("The key matrix have not modular inverse")
