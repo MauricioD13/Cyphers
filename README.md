bb880650fb10faf565d88404d540302589b11347
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-08-04 09:17:14 -0500

    Estoy listo para comenzar otro cifrado

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hill-Cypher
-Is a python code that encrypt message, also have GUI with tkinter
--- inversion_modular.py
+++
@@ -1,6 +0,0 @@
-def module_invert(determinant):
-    for i in range(0,abs(determinant)+1):
-        if (determinant*i)%26==1:
-            return i
-    else:
-            print("No tiene inverso modular")
