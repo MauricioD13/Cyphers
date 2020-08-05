2249b3e192ae32ea89b7d047179b8ea197664c5b
Mauricio David <mdavid_cuello@javeriana.edu.co>
2020-08-05 18:39:24 -0500

    Cifrado de Vigenere completo

--- Vigenere/Vigenere_Cypher.py
+++ Vigenere/Vigenere_Cypher.py
@@ -1,1 +1,44 @@
-Letters={"a":0,"b":1,"c":2,"d":3,"e":4,"f":5,"g":6,"h":7,"i":8,"j":9,"k":10,"l":11,"m":12,"n":13,"o":14,"p":15,"q":16,"r":17,"s":18,"t":19,"u":20,"v":21,"w":22,"x":23,"y":24,"z":25}
+
+def preparation(key,plaintext):
+    aux=key
+    while len(key)<len(plaintext):
+            key=key+aux
+    return key
+
+def Encrypt(key,plaintext):
+    key=key[0:len(plaintext)]
+    key_numbers=[]
+    plaintext_numbers=[]
+    cyphertext_list=[]
+    lenght_key=len(key)
+    cyphertext=""
+    for i in range(0,lenght_key):
+        key_numbers.append(ord(key[i]))
+        plaintext_numbers.append(ord(plaintext[i]))
+    for i in range(0,lenght_key):
+        cyphertext_list.append(chr(key_numbers[i]+plaintext_numbers[i]%127))
+        cyphertext=cyphertext+cyphertext_list[i]
+    print(cyphertext)
+    return cyphertext 
+def Decrypt(key,cyphertext):
+    key=list(key)
+    key_numbers=[]
+    cyphertext_numbers=[]
+    plaintext_list=[]
+    plaintext=""
+    lenght_key=len(key)
+    for i in range(0,lenght_key):
+        key_numbers.append(ord(key[i]))
+        cyphertext_numbers.append(ord(cyphertext[i]))
+    for i in range(0,lenght_key):
+        plaintext_list.append(chr(cyphertext_numbers[i]-key_numbers[i]%127))
+        plaintext=plaintext+plaintext_list[i]
+    return plaintext
+
+        
+key="hola"
+plaintext="quiero que esto funcione"
+key=preparation(key,plaintext)
+message=Encrypt(key,plaintext)
+message=Decrypt(key,message)
+print(message)
