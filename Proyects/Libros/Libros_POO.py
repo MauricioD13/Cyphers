d13888d598e180cf46abe45605ff4815514f1244
guasonito <guasonito@Mauro.168.0.6>
2021-01-30 19:03:07 -0500

    Correcciones Libreria POO

--- Proyects/Libros/Libros_POO.py
+++ Proyects/Libros/Libros_POO.py
@@ -15,25 +15,28 @@ class Library:
         output = []
         if option == 1: #Search by title
             for i in range(len(self.storage)):
-                if self.storage[i].title.lower()==word.lower():
-                    return books[i].title,books[i].autor,books[i].genre
-                    break
-            book01=self.storage[i].title.split()
-            for j in book01:
-                if word.lower()==j.lower():
+                if self.storage[i].title.lower() == word.lower():
+                    return self.storage[i].title,self.storage[i].autor,self.storage[i].genre
+                
+                book01=self.storage[i].title.split()
+                
+                for j in book01:
+                    if word.lower() == j.lower():
+                        
+                        output.append(self.storage[i])
+            return output
+        elif option == 2:
+            for i in range(len(self.storage)):
+                if self.storage[i].title.lower() == word.lower():
                     output.append(self.storage[i])
-        return output
-        if option == 2:
-            for i in range(len(books)-1):
-            if books[i][1].lower()==book_val.lower():
-                output.append(books[i])
-                #print("Titulo: {} \n Autor: {} \n Genero: {} \n Editorial: {}".format(books[i][0],books[i][1],books[i][2],books[i][3]))
-            book02 = books[i][1].split()
-            for j in book02:
-                if book_val.lower() == j.lower():
-                    #print("Titulo: {} \n Autor: {} \n Genero: {} \n Editorial: {}".format(books[i][0], books[i][1],
-                    output.append(books[i])                     
-        return output
+                    #print("Titulo: {} \n Autor: {} \n Genero: {} \n Editorial: {}".format(books[i][0],books[i][1],books[i][2],books[i][3]))
+                book02 = self.storage[i].title.split()
+                for j in book02:
+                    if word.lower() == j.lower():
+                        #print("Titulo: {} \n Autor: {} \n Genero: {} \n Editorial: {}".format(books[i][0], books[i][1],
+                        output.append(self.storage[i])   
+                        print(output)                  
+            return output
     def view_books(self):
         for i in self.storage:
             print(f"TITLE: {i.title} \t AUTOR: {i.autor} \n GENRE: {i.genre} \t EDITORIAL: {i.editorial}")
@@ -42,7 +45,12 @@ class Library:
 
     def load_books(self,file_name):
         items =[]
-        items = search_books.library()
+        with open("Libros.txt", 'r') as f:
+            for line in f:
+                info = line.split("-")
+                for i in range(len(info)):
+                    info[i] = info[i].strip()
+                items.append(info)
         for item in items:
             book = Book(item[0],item[1],item[2],item[3])
             library.new_book(book)
@@ -57,5 +65,11 @@ library = Library("Mi biblioteca")
 
 library.load_books("Libros.txt")
 
-library.view_books()
-library.search_books("El olvido",1)
+#library.view_books()
+
+books = []
+books = library.search_books("olvido",1)
+
+for book in books:
+    print(f"TITLE: {book.title} \t AUTOR: {book.autor} \n GENRE: {book.genre} \t EDITORIAL: {book.editorial}")
+    print("------------------------------------------")
