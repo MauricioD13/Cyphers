66e89d917463ba34e999af24f3973d6ef380bd77
guasonito <guasonito@Mauro.168.0.6>
2021-02-08 11:06:59 -0500

    Cifrado de Cesar terminado

--- Cyphers/caesar.py
+++ Cyphers/caesar.py
@@ -1,9 +1,10 @@
 
 letters = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"]
 
-word = input("Ingresar palabra: ")
+word = input("Enter word: ")
 word_numbers = []
 cypher_numbers = []
+precypher_numbers = []
 cypher_text = []
 shift = 12
 
@@ -16,11 +17,13 @@ for i in range(0,len(word)):
             word_numbers.append(j)
 
 for i in range(0,len(word_numbers)):
+    precypher_numbers.append(word_numbers[i]+shift)
     cypher_numbers.append((word_numbers[i]+shift)%26)
 
-print(word_numbers)
-print(cypher_numbers)
+print(f"Numbers of the word: {word_numbers}")
+print(f"Precypher: {precypher_numbers}")
+print(f"Cypher numbers: {cypher_numbers}")
             
 cypher_text = [letters[i] for i in cypher_numbers]
 
-print(cypher_text)
+print(f"Cypher text: {cypher_text}")
